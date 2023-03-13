723bfd2db469cb476557f0f45762301cf3dc7455
Lukas Bucher <lukas.bucher@hak-kitz.ac.at>
2023-03-13 16:40:23 +0100

    13.3
    Login

---
+++ Assets/SqlConnection.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data.MySqlClient;
+
+public class SqlConnection : MonoBehaviour
+{
+    string conString = "Server=web.hak-kitz.eu/dbv;Database=lukas.bucher_carsroyale;User ID=lukas.bucher;Password=password;";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
